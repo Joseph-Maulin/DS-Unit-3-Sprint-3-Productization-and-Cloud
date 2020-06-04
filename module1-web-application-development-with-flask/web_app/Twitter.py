9145c54f4c3a3021c02910ed012b9f42e2a63b4b
Joseph-Maulin <joseph.l.maulin@gmail.com>
2020-06-03 23:12:21 -0400

    tweet creator added

--- module1-web-application-development-with-flask/web_app/Twitter.py
+++ module1-web-application-development-with-flask/web_app/Twitter.py
@@ -8,17 +8,19 @@ migrate = Migrate()
 
 class User(db.Model):
 
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     user_name = db.Column(db.String(50))
 
     def __repr__(self):
         return f"<User {self.id} {self.user_name}>"
 
-class Tweets(db.Model):
+class Tweet(db.Model):
 
+    __tablename__ = 'tweets'
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer)
     tweet = db.Column(db.String(1000))
 
     def __repr__(self):
-        return f"<Tweets {self.id} {self.user_id} {self.tweet}>"
+        return f"<Tweet {self.id} {self.user_id} {self.tweet}>"
--- module1-web-application-development-with-flask/web_app/routes/twitter_routes.py
+++ module1-web-application-development-with-flask/web_app/routes/twitter_routes.py
@@ -1,7 +1,8 @@
 
 
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, jsonify
+from web_app.Twitter import User, Tweet, db
 
 
 twitter_routes = Blueprint("twitter_routes", __name__)
@@ -13,4 +14,35 @@ def tweet():
 
 @twitter_routes.route("/tweet/create", methods=["POST"])
 def add_tweet():
-    return "create tweet"
+
+    error = None
+
+    try:
+        user = db.session.query(User).filter(User.user_name == request.form['user']).all()
+        if user:
+            user_tweet = Tweet(user_id=int(user[0].id), tweet=request.form['tweet'])
+        else:
+            user = User(user_name=request.form['user'])
+            db.session.add(user)
+            user = db.session.query(User).filter(User.user_name == request.form['user']).all()
+            user_tweet = Tweet(user_id=int(user[0].id), tweet=request.form['tweet'])
+            print(user_tweet.user_id, user_tweet.tweet)
+
+        db.session.add(user_tweet)
+
+    except Exception as e:
+        error = e
+
+    if not error:
+        db.session.commit()
+        return jsonify({
+            "message": "TWEET ADDED OK",
+            "tweet" : dict(request.form)
+        })
+
+    else:
+        db.session.rollback()
+        return jsonify({
+            "message": "TWEET FAILED",
+            "tweet" : str(error)
+        })
